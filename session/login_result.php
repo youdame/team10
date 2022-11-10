62a8e2a18bd27ae039b3a249b31fd6a8d82311e7
Jinseo <2000jinseo@gmail.com>
2022-11-10 17:45:24 +0900

    [refactor] prepared statement

--- session/login_result.php
+++ session/login_result.php
@@ -6,17 +6,18 @@
     $input_id = $_POST['input_id'];
     $input_pw = $_POST['input_pw'];
 
-    $sql = "SELECT * FROM user WHERE id='$input_id'";
-    $result = $mysqli->query($sql);
+    $sql = "SELECT * FROM user WHERE id=? and pwd=?";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param("ss", $input_id, $input_pw);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
     if(mysqli_num_rows($result)){
         $row = $result->fetch_array(MYSQLI_ASSOC);
-
-        if($row!=null && $row['pwd']==$input_pw){
-            $_SESSION['id'] = $row['id'];
-            $_SESSION['name'] = $row['username'];
-            echo "<script>location.href='../test.php';</script>";
-            exit;
-        }
+        $_SESSION['id'] = $row['id'];
+        $_SESSION['name'] = $row['username'];
+        echo "<script>location.href='../test.php';</script>";
+        exit;
     }
     echo "<script>alert('Login failed.')</script>";
     echo "<script>location.href='login.php';</script>";
--- session/signup.php
+++ session/signup.php
@@ -14,16 +14,16 @@
         <table>
             <tr> 
                 <td>ID</td>
-                <td><input type='text' name='enter_id'/></td>
+                <td><input type='text' name='enter_id' required/></td>
                 <!--<td><button type="button" onclick="signup_chk_id.php">중복확인</button></td>-->
             </tr>
             <tr> 
                 <td>Password</td>
-                <td><input type='text' name='enter_pw'/></td>
+                <td><input type='text' name='enter_pw' required/></td>
             </tr>
             <tr> 
                 <td>Name</td>
-                <td><input type='text' name='enter_name'/></td>
+                <td><input type='text' name='enter_name' required/></td>
             </tr>
         </table>
         <input type="submit" value="Sign up">
