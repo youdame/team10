286fa3c7abc959e92f190fabff8ead0e5af91673
toulim <toulim0525@ewhain.net>
2022-11-12 22:33:45 +0900

    Update create.sql
    월별 영화 매출액 테이블 추가

--- create.sql
+++ create.sql
@@ -1,28 +1,60 @@
--- 홍진서 
 CREATE TABLE user(
-    id VARCHAR(15) NOT NULL PRIMARY KEY,
+    u_id VARCHAR(15) NOT NULL PRIMARY KEY,
     pwd VARCHAR(20) NOT NULL,
-    username VARCHAR(30) NOT NULL
+    username VARCHAR(30) NOT NULL,
+    usersex varchar(10) NOT NULL,
+    preferred VARCHAR(30)
 );
 
-CREATE TABLE movie_info (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    SN DECIMAL(30,0),
-    MOVIE_NM VARCHAR(200),
-    MNG_NM VARCHAR(200),
-    MAKR_NM VARCHAR(200),
-    IMPORT_CMPNY_NM VARCHAR(200),
-    DISTB_CMPNY_NM VARCHAR(200),
-    OPEN_DE VARCHAR(8),
-    MOVIE_TY_NM VARCHAR(200),
-    MOVIE_STLE_NM VARCHAR(200),
-    NLTY_NM VARCHAR(200),
-    WNTY_SCREEN_CO DECIMAL(28,5),
-    WNTY_SELNG_AM DECIMAL(28,5),
-    WNTY_AUDE_CO DECIMAL(28,5),
-    SU_SELNG_AM DECIMAL(28,5),
-    SU_AUDE_CO DECIMAL(28,5),
-    GENRE_NM VARCHAR(200),
-    GRAD_NM VARCHAR(200),
-    MOVIE_SE VARCHAR(30)
+CREATE INDEX index_username ON user(username);
+CREATE INDEX index_usersex_preferred ON user(usersex, preferred);
+
+
+CREATE TABLE movie_boxoffice(
+    m_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title varchar(50) NOT NULL,
+    released_date date NOT NULL,
+    sales bigint NOT NULL,
+    audience int NOT NULL,
+    screen_num int NOT NULL,
+    country	varchar(20) NOT NULL,
+    genre varchar(20) NOT NULL,
+    poster varchar(200) NOT NULL
+);
+
+
+CREATE TABLE user_like(
+    u_id VARCHAR(15),
+    m_id INT,
+    FOREIGN KEY (u_id) REFERENCES user(u_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (m_id) REFERENCES movie_boxoffice(m_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE rating(
+    m_id int,
+    rating decimal(2, 1) NOT NULL,
+    FOREIGN KEY (m_id) REFERENCES movie_boxoffice(m_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE director(
+    m_id int,
+    distributor varchar(50) NOT NULL,
+    director varchar(20) NOT NULL,
+    FOREIGN KEY (m_id) REFERENCES movie_boxoffice(m_id) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE movie_month 
+(
+    year	varchar(300),
+    month	varchar(300),
+    k_release	varchar(300),
+    k_screen	varchar(300),
+    k_sales	varchar(300),
+    k_audience	varchar(300),
+    k_share	varchar(300),
+    f_release	varchar(300),
+    f_screen	varchar(300),
+    f_sales	varchar(300),
+    f_audience	varchar(300),
+    f_share	varchar(300)
 );
